9452e22daadc80260a4ef601c1d592c582f3c7c2
Heipry <heipry@hotmail.com>
2020-05-23 16:33:33 +0200

    plural and variable translations

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -4,16 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Translation\TranslatorInterface;
 class DemoController extends AbstractController
 {
     /**
      * @Route("/demo", name="demo")
      */
-    public function index()
+    public function index(TranslatorInterface $translator)
     {
+        $translated = $translator->trans ('Hello');
+        dump($translated);
+        $name = 'Manolo';
+        $count = '2222';
+        $translated = $translator->trans(
+            'Hello %name%',
+            [ '%name%' => $name]
+        );
+        dump($translated);
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
+            'name' => $name,
+            'count' => $count,
         ]);
     }
 
